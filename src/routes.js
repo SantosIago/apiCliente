8b9db46341d66f175d21582943d65ca4c66a4042
Iago Santos <iagosantossud2019@gmail.com>
2024-04-25 23:54:08 -0300

    Criando endpoint findById

--- src/routes.js
+++ src/routes.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const ClienteController = require('./controller/clienteController');
 
 router.get('/clientes', ClienteController.findAll);
+router.get('/clientes/:id', ClienteController.findById);
 
 module.exports = router;
