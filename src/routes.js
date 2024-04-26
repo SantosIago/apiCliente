9fca3997504fe10001b1c68554e4b90e35710939
Iago Santos <iagosantossud2019@gmail.com>
2024-04-25 23:49:22 -0300

    Criando endpoint findAll

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const ClienteController = require('./controller/clienteController');
+
+router.get('/clientes', ClienteController.findAll);
 
 module.exports = router;
