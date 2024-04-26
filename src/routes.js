812f503d6debbe4b3d3ef2d6e1baeda8fc2cd764
Iago Santos <iagosantossud2019@gmail.com>
2024-04-26 00:13:11 -0300

    Criando endpoint Update

--- src/routes.js
+++ src/routes.js
@@ -6,5 +6,6 @@ const ClienteController = require('./controller/clienteController');
 router.get('/clientes', ClienteController.findAll);
 router.get('/clientes/:id', ClienteController.findById);
 router.post('/clientes', ClienteController.insert);
+router.put('/clientes/:id', ClienteController.update);
 
 module.exports = router;
